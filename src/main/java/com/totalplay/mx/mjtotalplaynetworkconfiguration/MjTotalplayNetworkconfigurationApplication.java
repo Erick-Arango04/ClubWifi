dd9758a28fbbefa410ef8d5d94d902c946adb52f
Erick Fernando <eriar1235@gmail.com>
2022-07-18 17:44:08 -0500

    first commit

---
+++ src/main/java/com/totalplay/mx/mjtotalplaynetworkconfiguration/MjTotalplayNetworkconfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.totalplay.mx.mjtotalplaynetworkconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjTotalplayNetworkconfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjTotalplayNetworkconfigurationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totalplay/mx/mjtotalplaynetworkconfiguration/MjTotalplayNetworkconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totalplay.mx.mjtotalplaynetworkconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjTotalplayNetworkconfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
